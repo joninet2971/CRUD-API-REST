fc01bf5950adba0d6edb97c0cc3d5a0dbcb0d822
joninet2971 <j.desplats@itecriocuarto.org.ar>
2025-04-01 08:54:06 +0000

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [formData, setFormData] = useState({
+    name: "",
+    features: "",
+    price: "",
+    year: "",
+  });
+  const [response, setResponse] = useState(null);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const data = {
+      name: formData.name,
+      data: {
+        features: formData.features,
+        price: Number(formData.price),
+        year: Number(formData.year),
+      },
+    };
+
+    fetch("https://api.restful-api.dev/objects", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((result) => {setResponse(result); localStorage.setItem("objectId", result.id); console.log("Objeto creado:", result);
+      })
+      .catch((error) => console.error("Error al enviar los datos", error));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Crear Objeto</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Nombre" required />
+        <input type="text" name="features" placeholder="Características" required />
+        <input type="number" name="price" placeholder="Precio" required />
+        <input type="number" name="year" placeholder="Año de fabricación" required />
+        <br />
+        <button type="submit">Enviar</button>
+      </form><br />
+      {response && (
+        <div>
+          <p><strong>Respuesta:</strong> {JSON.stringify(response)}</p>
+        </div>
+      )}
+    </div>
+  );
 }
-
-export default App
